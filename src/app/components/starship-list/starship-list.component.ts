4ed857b6487ae86944238c032b028d03dfd8ef95
Álvaro <alvarofmk@gmail.com>
2022-10-13 17:07:38 +0200

    Starship service & component created and programmed

--- src/app/components/starship-list/starship-list.component.ts
+++ src/app/components/starship-list/starship-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StarshipResponse } from 'src/app/interfaces/starship.interface';
+import { StarshipService } from 'src/app/services/starship.service';
 
 @Component({
   selector: 'app-starship-list',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./starship-list.component.css']
 })
 export class StarshipListComponent implements OnInit {
-
-  constructor() { }
-
+  
+  list: StarshipResponse = {} as StarshipResponse;
+  page: number = 1;
+  maxPages: number = 0;
+  imgUrlBase: string = "https://starwars-visualguide.com/assets/img/starships/"
+  
+  constructor(private starshipService: StarshipService) { }
+  
   ngOnInit(): void {
+    this.starshipService.getShips(this.page).subscribe(response =>{
+      this.list = response;
+      this.maxPages = Math.ceil(response.count/response.results.length);
+    }) 
   }
-
+  
+  public changePage(number: number){
+    this.page = number;
+    this.starshipService.getShips(this.page).subscribe(response => this.list = response);
+  }
+  
+  counter() {
+    return new Array(this.maxPages);
+  }
+  
+  handleMissingImage($evento: ErrorEvent) {
+    ($evento.target as HTMLImageElement).src = "https://starwars-visualguide.com/assets/img/placeholder.jpg";
+  }
 }
