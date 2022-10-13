6d58ebcbe8712e546c2076d9e27a57687abd2f92
AlejandroDV <aflowv@hotmail.com>
2022-10-13 17:58:52 +0200

    Species services and components

--- src/app/components/species-list/species-list.component.ts
+++ src/app/components/species-list/species-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SpecieResponse } from 'src/app/interfaces/specie.interface';
+import { SpecieService } from 'src/app/services/specie.service';
 
 @Component({
   selector: 'app-species-list',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpeciesListComponent implements OnInit {
 
-  constructor() { }
+  
+  list: SpecieResponse = {} as SpecieResponse;
+  page: number = 1;
+  maxPages: number = 0;
+  imgUrlBase: string = "https://starwars-visualguide.com/assets/img/species/"
+
+  constructor(private specieService: SpecieService) { }
 
   ngOnInit(): void {
+    this.specieService.getSpecies(this.page).subscribe(response =>{
+      this.list = response;
+      this.maxPages = Math.ceil(response.count/response.results.length);
+    }) 
+  }
+
+  public changePage(number: number){
+    this.page = number;
+    this.specieService.getSpecies(this.page).subscribe(response => this.list = response);
+  }
+
+  counter() {
+    return new Array(this.maxPages);
+  }
+
+  handleMissingImage($evento: ErrorEvent) {
+    ($evento.target as HTMLImageElement).src = "https://starwars-visualguide.com/assets/img/placeholder.jpg";
   }
 
 }
