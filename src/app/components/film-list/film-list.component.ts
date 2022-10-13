00128ace0ec39d894d5a2c4720a03ba83192e974
AlejandroDV <aflowv@hotmail.com>
2022-10-13 17:39:07 +0200

    Film service and component

--- src/app/components/film-list/film-list.component.ts
+++ src/app/components/film-list/film-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FilmResponse } from 'src/app/interfaces/film.interface';
+import { PeopleResponse } from 'src/app/interfaces/people.interface';
+import { FilmService } from 'src/app/services/film.service';
 
 @Component({
   selector: 'app-film-list',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmListComponent implements OnInit {
 
-  constructor() { }
+  list: FilmResponse = {} as FilmResponse;
+  page: number = 1;
+  maxPages: number = 0;
+  imgUrlBase: string ="https://starwars-visualguide.com/assets/img/films/";
+
+  constructor(private filmService: FilmService) { }
 
   ngOnInit(): void {
+    this.filmService.getFilms(this.page).subscribe(response=>{
+      this.list = response;
+      this.maxPages = Math.ceil(response.count/response.results.length)
+    })
   }
 
+  public changePage(number: number){
+    this.page = number;
+    this.filmService.getFilms(this.page).subscribe(response => this.list = response);
+  }
+
+  counter() {
+    return new Array(this.maxPages);
+  }
+  handleMissingImage($evento: ErrorEvent) {
+    ($evento.target as HTMLImageElement).src = "https://starwars-visualguide.com/assets/img/placeholder.jpg";
+  }
 }
